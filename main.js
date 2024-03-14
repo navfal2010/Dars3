72fff643c61a5888e45c4df6fd4cbe8311a3711e
navfal2010 <navfal808@gmail.com>
2024-03-14 11:26:45 +0500

    done

---
+++ main.js
@@ -0,0 +1,20 @@
+alert('Sizda hozir random sonlar chiqishini boshlaydi')
+
+let first = (Math.round(Math.random ()*100))
+
+alert(`${'Random orqali chiqan son:  '}${first}`)
+
+alert('Biz random soni 2 ga kopaytirmoqchi va bolmoqchimiz !')
+
+alert(`${'Random orqali chiqan son: '}${first}${' edi'}\n${'biz uni 2 kopaytirib: '}${first*2}${' sonini chiqardik,'}\n${'va shu soni 2 boloib: '}${first/2}${' sonini chiqardik,'}\n${'va shu soni 3 ga bolgandagi qoldiq '}${first%3}${' sonini chiqardik'}`)
+
+
+alert('Sizda hozir random sonlar chiqishini boshlaydi')
+
+let second = (Math.round(Math.random ()*100))
+
+alert(`${'Random orqali chiqan son:  '}${second}`)
+
+alert('Biz random soni 2 ga kopaytirmoqchi va bolmoqchimiz !')
+
+alert(`${'Random orqali chiqan son: '}${second}${' edi'}\n${'biz uni 2 kopaytirib: '}${second*2}${' sonini chiqardik,'}\n${'va shu soni 2 boloib: '}${second/2}${' sonini chiqardik,'}\n${'va shu soni 3 ga bolgandagi qoldiq '}${second%3}${' sonini chiqardik'}`)
